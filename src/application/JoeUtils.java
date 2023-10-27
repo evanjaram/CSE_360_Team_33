dbaa1a92672b67eeae1d5dba5ed5066a6836975b
jbcohen4 <144750171+jbcohen4@users.noreply.github.com>
2023-10-27 15:46:21 -0700

    Added JoeUtils

---
+++ src/application/JoeUtils.java
@@ -0,0 +1,26 @@
+package application;
+
+import javafx.scene.control.Alert;
+import javafx.scene.layout.Region;
+
+public class JoeUtils {
+	public static void showError(String message) {
+		new Alert(Alert.AlertType.ERROR, message).show();
+	}
+	
+	public static boolean isNumber(String input) {
+	    try {
+	        Double.parseDouble(input);
+	        return true;
+	    } catch (NumberFormatException e) {
+	        return false;
+	    }
+	}
+	
+	public static void setDimensions(Region thing, double x, double y, double width, double height) {
+	    thing.setLayoutX(x);
+	    thing.setLayoutY(y);
+	    thing.setPrefWidth(width);
+	    thing.setPrefHeight(height);
+	}
+}
