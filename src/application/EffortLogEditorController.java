d7c9ef03416f3e7a15b915d256af0457c6f778c9
jbcohen4 <144750171+jbcohen4@users.noreply.github.com>
2023-10-27 15:47:04 -0700

    Update EffortLogEditorController.java

--- src/application/EffortLogEditorController.java
+++ src/application/EffortLogEditorController.java
@@ -1,56 +1,37 @@
 package application;
 
-import java.util.ArrayList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-public class EffortLogViewerController {
-
+public class EffortLogEditorController {
 	@FXML
-	private AnchorPane anchor;
+	private TextField featureNameField;
+	@FXML
+	private TextField estimatedStoryPointsField;
+	@FXML
+	private TextField tagsField;
+	@FXML
+	private TextField hoursSpentField;
+	@FXML
+	private TextArea notes;
+	
+	
 	
 	@FXML
-	private void initialize() {
-		VBox vbox = new VBox();
-		ScrollPane scroll = new ScrollPane(vbox);
-		
-		JoeUtils.setDimensions(scroll, 22, 121, 560, 200);
-		anchor.getChildren().add(scroll);
-		vbox.setPrefWidth(scroll.getPrefWidth() - 20);
-		
-		ArrayList<String> features = new ArrayList<>();
-		for(int i = 1; i < 100; i++) {
-			features.add("Item " + i);
+	private void submitText() {
+		String hrs = hoursSpentField.getText();
+		if(!hrs.isEmpty() && !JoeUtils.isNumber(hrs)) {
+			JoeUtils.showError("hours spent must be a number");
+		} else {
+			Main.setScene("/EffortLogViewer.fxml");
 		}
-		
-		for(String item : features) {
-			Button button = new Button(item);
-		    button.setMaxWidth(Double.MAX_VALUE);
-		    button.setAlignment(Pos.CENTER_LEFT);
-		    vbox.getChildren().add(button);
-		}
-		
-		for(Node node : vbox.getChildren()) {
-		    node.setOnMouseClicked(event -> {
-		        if(event.getClickCount() == 2) {
-		        	Main.setScene("/EffortLogEditor.fxml");
-		        }
-		    });
-		}
-
-
 	}
+
 	
-	@FXML
-	private void goHome() {
-		Main.setScene("/Home.fxml");
-	}
+
 	
 }
