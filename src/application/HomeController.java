fa21a1304da7ae8624c640f35f7c260d6a0f6eb9
jbcohen4 <144750171+jbcohen4@users.noreply.github.com>
2023-10-29 13:14:55 -0700

    Update HomeController.java

--- src/application/HomeController.java
+++ src/application/HomeController.java
@@ -7,12 +7,29 @@ import javafx.fxml.FXML;
 public class HomeController {
 	
 	@FXML
-	private void goToEffortLogEditor() {
-		Main.setScene("/EffortLogEditor.fxml");
+	private void goToEffortLogConsole() {
+		Main.setScene("EffortLoggerGUI.fxml");
 	}
 	
 	@FXML
 	private void goToEffortLogViewer() {
 		Main.setScene("/EffortLogViewer.fxml");
 	}
+	
+	@FXML
+	private void goToPublicLogs() {
+		Main.setScene("/PublicLogs.fxml");
+	}
+	
+	@FXML
+	private void goToDefectLogEditor() {
+		System.out.println("That scene is not yet implemented");
+	}
+	
+	@FXML
+	private void goToDefinitions() {
+		System.out.println("That scene is not yet implemented");
+	}
+	
+	
 }
