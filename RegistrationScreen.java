ad38d8b09aefb59bfa697c96971299b790f61872
Logan <lehunger@asu.edu>
2023-10-29 14:32:06 -0700

    controller updates

---
+++ RegistrationScreen.java
@@ -0,0 +1,90 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+
+public class RegistrationScreen {
+	//Registration GUI Components
+	@FXML
+	private AnchorPane registrationScene;
+	@FXML
+	private Circle circleLogo;
+	@FXML
+	private Line checkLogo1, checkLogo2;
+	@FXML
+	private Label nameLogo, sceneLabel, usernameLabel, emailLabel, passwordLabel1, passwordLabel2;
+	@FXML
+	private Label successLabel, emptyFieldWarning, diffPasswordWarning;
+	@FXML
+	private TextField usernameField, emailField, passwordField1, passwordField2;
+	@FXML
+	private Button registerButton, leaveSceneButton;
+	private static String username, email, password, rePassword;
+	
+	@FXML
+	//initializes GUI components
+	public void initialize() {
+		//initialize anchorpane
+		registrationScene = new AnchorPane();
+		
+		//attaches buttons to handlers
+		registerButton.setOnAction(this::registerButtonHandler);
+		leaveSceneButton.setOnAction(this::leaveSceneButtonHandler);
+		
+		//hide registration notification labels from user initially
+		successLabel.setVisible(false);
+		emptyFieldWarning.setVisible(false);
+		diffPasswordWarning.setVisible(false);
+	}
+	
+	//username field handler for inputting username registration info
+	public void usernameFieldHandler(ActionEvent event) {
+		username = usernameField.getText();
+	}
+	
+	//email field handler for inputting email registration info
+	public void emailFieldHandler(ActionEvent event) {
+		email = emailField.getText();
+	}
+	
+	//first password field handler for inputting password for first time
+	public void passwordField1Handler(ActionEvent event) {
+		password = passwordField1.getText();
+	}
+	
+	//second password field handler to verify first password field matches second field
+	public void passwordField2Handler(ActionEvent event) {
+		rePassword = passwordField2.getText();
+	}
+	
+	//confirms info is correctly entered and displays registration confirmation pop-up
+	public void registerButtonHandler(ActionEvent event) {
+		if(username != null && email != null && password != null && rePassword != null) {
+			if(password.equals(rePassword)) {
+				successLabel.setVisible(true);
+				emptyFieldWarning.setVisible(false);
+				diffPasswordWarning.setVisible(false);
+			}else {
+				diffPasswordWarning.setVisible(true);
+				successLabel.setVisible(false);
+				emptyFieldWarning.setVisible(false);
+			}
+		}else {
+			emptyFieldWarning.setVisible(true);
+			successLabel.setVisible(false);
+			diffPasswordWarning.setVisible(false);
+		}
+	}
+	
+	//return user to login screen with or without registration
+	public void leaveSceneButtonHandler(ActionEvent event) {
+		Main.setScene("/LoginScreen.fxml");
+	}
+
+}
---
+++ TwoFactorAuthentication.java
@@ -0,0 +1,104 @@
+package application;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+
+public class TwoFactorAuthentication {
+	//2FA GUI Components
+	@FXML
+	private AnchorPane twoFAScene;
+	@FXML
+	private Circle circleLogo;
+	@FXML
+	private Line checkLogo1, checkLogo2;
+	@FXML
+	private Label nameLogo, sceneLabel, emailNotifLabel, timerLabel;
+	@FXML
+	private VBox vb;
+	@FXML
+	private TextField keyField;
+	@FXML
+	private Button verifyButton;
+	//private static Timer timer;
+	private static int delay, interval;
+	private ScheduledExecutorService scheduler;
+	private ScheduledFuture<?> timer;
+	
+	@FXML
+	//initializes GUI components
+	public void initialize() {
+		twoFAScene = new AnchorPane();
+		twoFAScene.getChildren().add(timerLabel);
+		
+		//connects keyField to handler and connects verifyButton to button handler within key handler
+		keyField = new TextField();
+		keyField.setOnAction(this::keyFieldHandler);
+		
+		//starting state of timer
+		timerLabel = new Label("1:00");
+		timerLabel.setVisible(true);
+		
+		//instantiate timerHandler to start timer and connect it to timerLabel
+		delay = 1;
+		interval = 59;
+		timerHandler();
+	}
+	
+	//handler for generating random key for 2FA
+	public String keyGenerator(int keyLength) {
+		String keyChars = "abcdefghijklmnopqrstuvwxyz" + "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789";
+		StringBuilder keyBuilder = new StringBuilder(keyLength);
+		
+		for(int i = 0; i <= keyLength+1; i++) {
+			int index = (int) (keyChars.length() * Math.random());
+			keyBuilder.append(keyChars.charAt(index));
+		}
+		
+		System.out.println(keyBuilder.toString());
+		return keyBuilder.toString();
+	}
+	
+	//key password field event handler for inputting key
+	public void keyFieldHandler(ActionEvent event) {
+		String key = keyGenerator(7);
+		
+		while(!verifyButton.isPressed()) {
+			if(keyField.getText().equals(key)) {
+				verifyButton.setOnAction(this::verifyButtonHandler);
+				break;
+			}
+		}
+	}
+	
+	//verification button event handler to move to home scene
+	public void verifyButtonHandler(ActionEvent event) {
+		Main.setScene("/Home.fxml");
+	}
+	
+	//counts down timer and returns to login screen if reaches 0
+	private void timerHandler() {
+		scheduler = Executors.newScheduledThreadPool(1);
+		Runnable countdown = () -> timerLabelDisplay();
+		timer = scheduler.scheduleWithFixedDelay(countdown, delay, delay, TimeUnit.SECONDS);
+		Runnable endCountdown = () -> timer.cancel(true);
+		scheduler.schedule(endCountdown, 61, TimeUnit.SECONDS);
+		Main.setScene("/LoginScreen.fxml");
+	}
+	
+	private void timerLabelDisplay() {
+		timerLabel.setText(String.format("0:%d", interval));
+		--interval;
+	}
+}
