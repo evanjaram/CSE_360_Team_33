902bd24c38959bf478c462686b06c9d201b18c43
jbcohen4 <144750171+jbcohen4@users.noreply.github.com>
2023-10-29 20:19:22 -0700

    Add files via upload

---
+++ FeatureInfo.java
@@ -0,0 +1,30 @@
+package application;
+
+// Author: Joseph Cohen
+
+// Defines the information that will be associated with each feature.
+
+public class FeatureInfo {
+	public String name;
+	public String tags;
+	public String notes;
+	public double esimatedStoryPoints;
+	public double hoursSpent;
+	
+	
+	public FeatureInfo(String name, String tags, String notes, double estStoryPoints, double hrsSpent) {
+		this.name = name;
+		this.tags = tags;
+		this.notes = notes;
+		this.esimatedStoryPoints = estStoryPoints;
+		this.hoursSpent = hrsSpent;
+	}
+	
+	public FeatureInfo(String name) {
+		this.name = name;
+		this.tags = "";
+		this.notes = "";
+		this.esimatedStoryPoints = -1.0;
+		this.hoursSpent = -1.0;
+	}
+}
