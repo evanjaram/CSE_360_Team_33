9e62efabc021f4d032202f893bea0e7d7d8a4b10
jbcohen4 <jbcohen4@asu.edu>
2023-10-31 11:56:42 -0700

    wrote a test comment

--- src/application/JoeUtils.java
+++ src/application/JoeUtils.java
@@ -3,6 +3,8 @@ package application;
 import javafx.scene.control.Alert;
 import javafx.scene.layout.Region;
 
+
+//Making a test comment to see if command line github can work for me
 public class JoeUtils {
 	public static void showError(String message) {
 		new Alert(Alert.AlertType.ERROR, message).show();
